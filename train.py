6c81343630c2120d0d83fbad8e8d574181c12e7b
LeDatPhucAn <2015phucan@gmail.com>
2026-03-26 00:28:49 +0700

    get ready for training

--- train.py
+++ train.py
@@ -28,32 +28,31 @@ torch.backends.cudnn.benchmark = True
 def train_fn(train_loader, model, optimizer, loss_fn, scaler, scaled_anchors):
     loop = tqdm(train_loader, leave=True)
     losses = []
-    x, y = next(iter(train_loader))
-    # for batch_idx, (x, y) in enumerate(loop):
-    x = x.to(config.DEVICE)
-    y0, y1 = (
-        y[0].to(config.DEVICE),
-        y[1].to(config.DEVICE),
-        # y[2].to(config.DEVICE),
-    )
-
-    with torch.cuda.amp.autocast():
-        out = model(x)
-        loss = (
-            loss_fn(out[0], y0, scaled_anchors[0])
-            + loss_fn(out[1], y1, scaled_anchors[1])
-            # + loss_fn(out[2], y2, scaled_anchors[2])
+    for batch_idx, (x, y) in enumerate(loop):
+        x = x.to(config.DEVICE)
+        y0, y1 = (
+            y[0].to(config.DEVICE),
+            y[1].to(config.DEVICE),
+            # y[2].to(config.DEVICE),
         )
 
-    losses.append(loss.item())
-    optimizer.zero_grad()
-    scaler.scale(loss).backward()
-    scaler.step(optimizer)
-    scaler.update()
+        with torch.cuda.amp.autocast():
+            out = model(x)
+            loss = (
+                loss_fn(out[0], y0, scaled_anchors[0])
+                + loss_fn(out[1], y1, scaled_anchors[1])
+                # + loss_fn(out[2], y2, scaled_anchors[2])
+            )
 
-    # update progress bar
-    mean_loss = sum(losses) / len(losses)
-    loop.set_postfix(loss=mean_loss)
+        losses.append(loss.item())
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # update progress bar
+        mean_loss = sum(losses) / len(losses)
+        loop.set_postfix(loss=mean_loss)
 
 
 
@@ -91,7 +90,7 @@ def main():
         #print("On Train loader:")
         #check_class_accuracy(model, train_loader, threshold=config.CONF_THRESHOLD)
 
-        if epoch > 0 and epoch % 250 == 0:
+        if epoch > 0 and epoch % 50 == 0:
             check_class_accuracy(model, test_loader, threshold=config.CONF_THRESHOLD)
             pred_boxes, true_boxes = get_evaluation_bboxes(
                 test_loader,
